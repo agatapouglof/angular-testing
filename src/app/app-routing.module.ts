90d4d967855455ef140dcc182f0a3a9173de6754
agatapouglof <001ndj@gmail.com>
2019-11-28 09:58:41 +0100

    add integration test work files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserDetailsComponent } from './2.3-user-details/user-details.component';
+import { UsersComponent } from './2.users/users.component';
+import { TodosComponent } from './2.2-todos/todos.component';
+import { HomeComponent } from './2.home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users/:id', component: UserDetailsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
